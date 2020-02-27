de251964f8d9351f78a1b831d013d01b73e5b302
Piotr Szczepanski <ask.piotr@yahoo.co.uk>
2020-02-27 16:21:26 +0100

    list all_posts functionality added

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-# from .models import Blog
+from .models import Post
 
 def all_posts(request):
-    # blogs = Blog.objects.order_by('-date')
-    # return render(request, 'app_blog_all_posts/all_posts.html', {'blogs':blogs})
-    return render(request, 'app_blog_all_posts/all_posts.html')
+    # order by most recent
+    posts = Post.objects.order_by('-date')
+    
+    # order by most recent and limit to last 5 blogs, here next blog functionality would have to be added
+    # posts = Post.objects.order_by('-date')[:5]
+    
+    return render(request, 'app_blog_all_posts/all_posts.html', {'posts':posts})
 
 # def detail(request, blog_id):
 #     blog = get_object_or_404(Blog, pk=blog_id)
